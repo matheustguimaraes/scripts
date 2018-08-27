0a6d6a8509dd97f18fd5736a82b4b4aac57de7ab
Matheus Guimarães <matheusguimr@gmail.com>
2018-08-27 09:42:07 -0300

    Fix mistake in unzip command

--- Ubuntu_16.04/InstallOpenCV.sh
+++ Ubuntu_16.04/InstallOpenCV.sh
@@ -1,5 +1,5 @@
 OPENCV_VERSION='3.4.2'
-current_username="$USER"
+CURRENT_USERNAME="$USER"
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -7,40 +7,32 @@ sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
 
 sudo apt-get install -y build-essential cmake
-
 sudo apt-get install -y qt5-default libvtk6-dev
-
 sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
-
 sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
-
 sudo apt-get install -y libtbb-dev libeigen3-dev
-
 sudo apt-get install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
-
 sudo apt-get install -y ant default-jdk
-
 sudo apt-get install -y doxygen
-
 sudo apt-get install build-essential libgtk2.0-dev libjpeg-dev libtiff5-dev libjasper-dev libopenexr-dev cmake python-dev python-numpy python-tk libtbb-dev libeigen3-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev libqt4-dev libqt4-opengl-dev sphinx-common texlive-latex-extra libv4l-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev default-jdk ant libvtk5-qt4-dev
 
 sudo apt-get install -y unzip wget
 
 wget https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
-unzip opencv-${OPENCV_VERSION}.zip
-rm opencv-${OPENCV_VERSION}.zip
+unzip ${OPENCV_VERSION}.zip
+rm ${OPENCV_VERSION}.zip
 mv opencv-${OPENCV_VERSION} OpenCV
 
 wget https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip
-unzip opencv_contrib-${OPENCV_VERSION}.zip
-rm opencv_contrib-${OPENCV_VERSION}.zip
+unzip ${OPENCV_VERSION}.zip
+rm ${OPENCV_VERSION}.zip
 mv opencv_contrib-${OPENCV_VERSION} OpenCV_contrib
 
 cd OpenCV
 mkdir build
 cd build
 
-cmake -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_VTK=ON .. -DCMAKE_BUILD_TYPE=RELEASE -DOPENCV_EXTRA_MODULES_PATH=/home/${current_username}/Downloads/OpenCV_contrib/modules ..
+cmake -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_VTK=ON .. -DCMAKE_BUILD_TYPE=RELEASE -DOPENCV_EXTRA_MODULES_PATH=/home/${CURRENT_USERNAME}/Downloads/OpenCV_contrib/modules ..
 
 make
 sudo make install
