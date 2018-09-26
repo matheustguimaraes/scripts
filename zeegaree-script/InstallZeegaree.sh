89b35983ac26a171ddfa54909bffc1e15655beb2
Matheus Guimarães <matheusguimr@gmail.com>
2018-09-26 08:47:12 -0300

    Fix mistake in unzip command

--- zeegaree-script/InstallZeegaree.sh
+++ zeegaree-script/InstallZeegaree.sh
@@ -6,9 +6,11 @@ sudo apt-get install gir1.2-notify-0.7
 sudo apt-get install python-dbus
 
 wget https://github.com/mivoligo/Zeegaree/archive/master.zip
-unzip Zeegaree-master.zip
-rm Zeegaree-master.zip
+unzip master.zip
+rm master.zip
 mv Zeegaree-master Zeegaree
 
-printf "For run, just type 'cd Zeegaree/'\n"
-printf "And 'python zeeragee.py'\n"
+echo "----------------------------------------------------"
+echo "For execute the program, just enter 'cd Zeegaree/'\n"
+echo "And 'python zeeragee.py'\n"
+echo "----------------------------------------------------"
