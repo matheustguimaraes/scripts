68fbd4d2a1986ffc73bac77c32d45c2d1120d9c4
Matheus Guimarães <matheusguimr@gmail.com>
2018-12-01 17:29:04 -0300

    Add git shortcuts

---
+++ git-script/git-script.sh
@@ -0,0 +1,6 @@
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.df diff
+git config --global alias.dfs "diff --staged"
+git config --global alias.logg "log --graph --decorate --oneline --abbrev-commit --all"
