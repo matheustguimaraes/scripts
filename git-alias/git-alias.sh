1d72e09866f48c49bdbd0536cebf7380a296bee4
Matheus Guimaraes <matheusguimr@gmail.com>
2020-01-15 14:21:17 -0300

    Change git logg, to show commit msg, date and author name

--- git-alias/git-alias.sh
+++ git-alias/git-alias.sh
@@ -4,4 +4,4 @@ git config --global alias.ci commit
 git config --global alias.br branch
 git config --global alias.df diff
 git config --global alias.dfs "diff --staged"
-git config --global alias.logg "log --graph --decorate --oneline --abbrev-commit --all"
+git config --global alias.logg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
